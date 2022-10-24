8a566d85d336b0c36602efce3a455f692076f63f
terrence-brian <86241569+terrence-brian@users.noreply.github.com>
2022-10-24 19:21:23 +0300

    Update FizzBuzz.cpp

--- Week 1/FizzBuzz.cpp
+++ Week 1/FizzBuzz.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> answer;
+        for(int i=1; i<=n; i++){
+            int a, b;
+            a = (i%3==0);
+            b = (i%5==0);
+            
+            if(a && b){
+                answer.push_back("FizzBuzz");
+            }
+            else if(a){
+                answer.push_back("Fizz");
+            }
+            else if(b){
+                answer.push_back("Buzz");
+            }
+            else{
+                answer.push_back(to_string(i));
+            }
+        }
+        return answer;
+    }
+};
