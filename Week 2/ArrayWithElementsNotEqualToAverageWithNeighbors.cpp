c633529e65531859d69ac3f5d873a77f4cebd6b4
terrence-brian <86241569+terrence-brian@users.noreply.github.com>
2022-11-14 18:28:12 +0300

    Update ArrayWithElementsNotEqualToAverageWithNeighbors.cpp

--- Week 2/ArrayWithElementsNotEqualToAverageWithNeighbors.cpp
+++ Week 2/ArrayWithElementsNotEqualToAverageWithNeighbors.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=1;i<n-1;i++)
+        int n = nums.size();
+        for(int i=1; i<n-1; i++)
         {
-            if((nums[i-1]<nums[i] && nums[i]<nums[i+1]) || (nums[i-1]>nums[i] && nums[i]>nums[i+1]))
-                swap(nums[i],nums[i+1]);
+            if((nums[i-1] < nums[i] && nums[i] < nums[i+1]) || (nums[i-1] > nums[i] && nums[i] > nums[i+1]))
+                swap(nums[i], nums[i+1]);
         }
         return nums;
     }
